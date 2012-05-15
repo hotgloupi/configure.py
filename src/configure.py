e94cfc9d99b95714e06a1e357383fb1eb2926974
Raphael Londeix <raphael.londeix@gmail.com>
2012-05-15 02:47:02 +0200

    use sources and targets definitions

--- src/configure.py
+++ src/configure.py
@@ -3,11 +3,20 @@
 import argparse
 import os, sys
 
-ROOT_DIR = os.path.dirname(__file__)
+def cleanpath(p):
+    p = os.path.normpath(p).replace('\\', '/')
+    if p.startswith('./'):
+        return p[2:]
+    return p
+
+def cleanjoin(*args):
+    return cleanpath(os.path.join(*args))
+
+ROOT_DIR = cleanpath(os.path.dirname(__file__))
 PROJECT_CONF_DIR_NAME = ".config"
-PROJECT_CONF_DIR = os.path.join(ROOT_DIR, PROJECT_CONF_DIR_NAME).replace('\\', '/')
-PROJECT_CONF_TEMPLATES_DIR = os.path.join(PROJECT_CONF_DIR, 'templates').replace('\\', '/')
-PROJECT_CONF_FILE = os.path.join(PROJECT_CONF_DIR, "project.py").replace('\\', '/')
+PROJECT_CONF_DIR = cleanjoin(ROOT_DIR, PROJECT_CONF_DIR_NAME)
+PROJECT_CONF_TEMPLATES_DIR = cleanjoin(PROJECT_CONF_DIR, 'templates')
+PROJECT_CONF_FILE = cleanjoin(PROJECT_CONF_DIR, "project.py")
 PROJECT_CONF_FILE_TEMPLATE = """# -*- encoding: utf-8 -*-
 
 # The project name (this is not the version code name).
@@ -23,7 +32,7 @@ NAME = %(project_name)s
 ##      >>> VERSION_NAME_CMD = ["git", "--describe"]            # safer syntax
 ##      >>> def myPythonFunction() : return 32                  # simple function
 ##      ...
-##      >>> VERSION_NAME_CMD = myPythonFunction
+##      >>> VERSION_NAME_CMD = _myPythonFunction
 ##
 
 # The version name (could be "alpha" for example)
@@ -38,10 +47,10 @@ DEFAULT_BUILD_TYPE = 'Debug'
 TUP_TEMPLATE_PATH = %(project_conf_tup_file)s
 TUP_TEMPLATE_PATH_CMD = None
 
-# Tup template (used when TUP_TEMPLATE_PATH is None)
-# Can be a python function or a shell command that accept a
-# relative directory path as only argument and returns (or print out)
-# the tup file template.
+# Tup template generator (used when TUP_TEMPLATE_PATH is None).
+# Can be a python function or a shell command that accept a relative directory
+# path as only argument and respectively returns or print out the tup file
+# template.
 TUP_TEMPLATE_GEN = None
 TUP_TEMPLATE_GEN_CMD = None
 
@@ -55,30 +64,41 @@ TUPRULES_TEMPLATE_PATH_CMD = None
 # to change that.
 #
 SOURCE_DIRECTORIES = [
-    '.',                            # project root dir
+    'src',                            # project root dir
 ]
 
 # Source directories are treated as tree. If you set this to False, you'll have
 # to declare every directory, not just root.
 RECURSE_OVER_SOURCE_DIRECTORIES = True
 
-#
+###############################################################################
 # Targets is a list that contains high level targets description. Each target
-# is described through a dictionary that contains at least three keys:
-#   - 'input_files' (required): Contains a list input files. (can contains
-#                               wildcard, see tup manual).
+# is described through a dictionary that contains the following keys:
+#   - 'input_items' (required): Contains a list input items (see below).
+#   - 'additional_inputs' (optional): Contains a list of additional input items
 #   - 'command' (required) : The command line to be inserted in the tup rule.
 #   - 'output_file' (required): out file name.
 #   - 'additional_output_files (optional): a list of additional generated files.
 #   - 'output_directory' (optional): where to build out files (defaults to build dir).
 #
+
+import sys as _sys
+
+# Convenient function to get a portable executable name
+def executable(name):
+    return name + (_sys.platform.startswith('win') and '.exe' or '')
+
 TARGETS = [
     {
-        'input_files': ['*.o'],
-        'output_file': my_program
-        'output_directory': 'release/bin'
+        'input_items': [('src', '*.o')],
+        'command': 'link_cpp_executable',
+        'output_file': executable('my_program'),
+        'output_directory': 'release/bin',
     },
 ]
+
+# Same as before
+RECURSE_OVER_TARGET_DIRECTORIES = True
 
 ###############################################################################
 ## This section provides examples on how extending project variables (delete or
@@ -88,49 +108,56 @@ TARGETS = [
 CC = "gcc"
 CXX = "g++"
 
-import sysconfig
+import sysconfig as _sysconfig
 
-PYTHON_DIR          = sysconfig.get_config_var('prefix')
+PYTHON_DIR          = _sysconfig.get_config_var('prefix')
 PYTHON_INCLUDE_DIR  = PYTHON_DIR + "/include"
 PYTHON_LIBRARY_DIR  = PYTHON_DIR + "/libs"
+
+
 """
 
-PROJECT_CONF_TUP_FILE = os.path.join(
+PROJECT_CONF_TUP_FILE = cleanjoin(
     PROJECT_CONF_TEMPLATES_DIR,
     'Tupfile.templates'
-).replace('\\', '/')
+)
 PROJECT_CONF_TUP_FILE_TEMPLATE = """
 include_rules
 
+: foreach {SOURCE_DIR}/*.cpp |> !make_cpp_object |>
+
 """
 
-PROJECT_CONF_TUPRULES_FILE = os.path.join(
+PROJECT_CONF_TUPRULES_FILE = cleanjoin(
     PROJECT_CONF_TEMPLATES_DIR,
     'Tuprules.tup.template'
-).replace('\\', '/')
+)
 PROJECT_CONF_TUPRULES_FILE_TEMPLATE = """
 # This file is generated in each build directory.
 # All project variables and the following ones are defined:
 #   * BUILD_TYPE
 #
 
-BUILD_TYPE = %(BUILD_TYPE)s
+BUILD_TYPE = {BUILD_TYPE}
 BUILD_DIR = $(TUP_CWD)
 SOURCE_DIR = $(BUILD_DIR)/..
 
-CC = %(CC)s
-CXX = %(CXX)s
+CC = {CC}
+CXX = {CXX}
 
-CFLAGS      +=  -I%(PYTHON_INCLUDE_DIR)s
+CFLAGS      +=  -I{PYTHON_INCLUDE_DIR}
 CXXFLAGS    +=  -std=c++0x
-!make_cpp_object = |> $(CXX) $(CCFLAGS) $(CXXFLAGS) $(CXXFLAGS_%%f) -c %%f -o %%o |> %%b.o
+LDFLAGS     +=  -L{PYTHON_LIBRARY_DIR} -lpython32
+!make_cpp_object = |> $(CXX) $(CFLAGS) $(CXXFLAGS) $(CXXFLAGS_%f) -c %f -o %o |> %b.o
+!link_cpp_executable = |> $(CXX) %f $(LDFLAGS) $(LDFLAGS_%f) -o %o |>
 
 """
+
 
 def detectDefaults():
     defaults = {}
     defaults['project_conf_dir_name'] = PROJECT_CONF_DIR_NAME
-    defaults['project_name'] = os.path.basename(os.path.abspath(ROOT_DIR)).replace('\\', '/')
+    defaults['project_name'] = os.path.basename(os.path.abspath(ROOT_DIR))
     defaults['project_version_name'] = 'alpha'
     defaults['project_version_name_cmd'] = None
     defaults['project_conf_tup_file'] = PROJECT_CONF_TUP_FILE
@@ -175,7 +202,7 @@ class ConfProxy:
             if var is None:
                 raise ValueError("Result of command %s_CMD is None" % key)
         if isinstance(var, str):
-            return var.replace('\\', '/')
+            return cleanpath(var)
         return var
 
     def __getitem__(self, key):
@@ -222,31 +249,150 @@ def parseArguments(project):
     return parser.parse_args()
 
 class BuildEnv:
+
+    _required_target_keys = [
+        'input_items', 'command', 'output_file'
+    ]
+
+    _available_target_keys = _required_target_keys + [
+        'additional_input_files',
+        'additional_output_files',
+        'output_directory',
+    ]
+
     def __init__(self, project, args):
         self._project = project
         self._build_type = args.build_type.lower()
-        self._build_dir = args.build_dir
+        self._build_dir = cleanjoin(ROOT_DIR, args.build_dir)
 
     def _setupDir(self):
         if not os.path.exists(self._build_dir):
             os.mkdir(self._build_dir)
 
-    @property
-    def project_dict(self):
-        p = {}
-        p.update(self._project)
-        p['BUILD_TYPE'] = self._build_type
-        return p
+    def _getProjectDict(self, **kwargs):
+        kwargs.update(self._project)
+        return kwargs
 
     def _setupTuprules(self):
         with open(self._project.TUPRULES_TEMPLATE_PATH, 'r') as f:
             template = f.read()
+        project_dict = self._getProjectDict(
+            BUILD_TYPE=self._build_type
+        )
         with open(os.path.join(self._build_dir, 'Tuprules.tup'), 'w') as f:
-            f.write(template % self.project_dict)
+            f.write(template.format(**project_dict))
+
+    def _setupTupfile(self, source_dir, build_dir):
+        tup_file = os.path.join(build_dir, 'Tupfile')
+        print(
+            '--',
+            os.path.exists(tup_file) and "Updating" or "Creating",
+            "'%s'." % cleanpath(tup_file)
+        )
+        template_path = self._project.TUP_TEMPLATE_PATH
+        with open(template_path, 'r') as f:
+            template = f.read()
+        project_dict = self._getProjectDict(
+            SOURCE_DIR=cleanpath(os.path.relpath(source_dir, start=build_dir)),
+        )
+        with open(tup_file, 'w') as f:
+            f.write(template.format(**project_dict))
+
+    def _setupSourceDirectory(self, rel_source_dir):
+        source_dir = cleanjoin(ROOT_DIR, rel_source_dir)
+        if not os.path.exists(source_dir):
+            raise Exception("The folder '%s' does not exists" % source_dir)
+        assert os.path.isdir(source_dir)
+        if os.path.samefile(source_dir, ROOT_DIR):
+            raise Exception(
+                "The root directory '%s' cannot be a source directory" % os.path.abspath(ROOT_DIR)
+            )
+
+        for root, dirnames, filenames in os.walk(source_dir):
+            build_dir = os.path.join(self._build_dir, rel_source_dir)
+            if not os.path.exists(build_dir):
+                os.makedirs(build_dir)
+            self._setupTupfile(source_dir, build_dir)
+
+    def _parseTargetInputs(self, inputs, output_directory):
+        for input_ in inputs:
+            if isinstance(input_, str):
+                print("str:", input_)
+                yield cleanpath(os.path.relpath(
+                    os.path.join(self._build_dir, input_),
+                    start=output_directory
+                ))
+            elif isinstance(input_, tuple):
+                assert len(input_) == 2
+                build_dir, file_ = (cleanjoin(self._build_dir, input_[0]), cleanpath(input_[1]))
+                assert '/' not in file_
+                for root, dirnames, filenames in os.walk(build_dir):
+                    yield '%s/%s' % (
+                        cleanpath(os.path.relpath(root, start=output_directory)),
+                        file_
+                    )
+                    if not self._project.RECURSE_OVER_TARGET_DIRECTORIES:
+                        break
+            else:
+                raise ValueError("Unknown item type : %s" % str(input_))
+
+    def _parseTarget(self,
+                     input_items=None,
+                     additional_input_files=None,
+                     command=None,
+                     output_file=None,
+                     additional_output_files=None,
+                     output_directory=None):
+        output_directory = cleanjoin(self._build_dir, output_directory or '.')
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
+
+        # backslash + linefeed padded
+        bslf = lambda s: '%s\\\n' % (len(s) < 79 and (s + (79 - len(s)) * ' ') or s)
+
+        result = ':\\\n'
+        for input_ in  self._parseTargetInputs(input_items, output_directory):
+            result += bslf('  %s' % input_)
+
+        if additional_input_files is not None:
+            raise NotImplemented()
+
+        result += bslf('    |> !%s' % command)
+
+        result += bslf('    |> %s' % output_file)
+
+        if additional_output_files is not None:
+            raise NotImplemented()
+
+        result += '\n'
+        return (output_directory, result)
 
     def setup(self):
         self._setupDir()
         self._setupTuprules()
+        for source_dir in self._project.SOURCE_DIRECTORIES:
+            self._setupSourceDirectory(source_dir)
+        target_dirs = {}
+        for target in self._project.TARGETS:
+            for k in self._required_target_keys:
+                if not k in target:
+                    raise KeyError("Missing key '%s' for target %s" % (k, str(target)))
+            for k in target.keys():
+                if not k in self._available_target_keys:
+                    raise KeyError("Invalid key '%s' for target %s" % (k, str(target)))
+            dir_, res = self._parseTarget(**target)
+            target_dirs.setdefault(dir_, []).append(res)
+        for target_dir, rules in target_dirs.items():
+            content = 'include_rules\n\n' + ('\n' + '#' * 80 + '\n').join(rules)
+            tup_file = cleanjoin(target_dir, 'Tupfile')
+            print(
+                '--',
+                os.path.exists(tup_file) and 'Updating' or 'Creating',
+                "'%s'." % tup_file
+            )
+            with open(tup_file, 'w') as f:
+                f.write(content)
+
 
 def main():
     if needBootstrap():
@@ -254,7 +400,7 @@ def main():
     assert os.path.isdir(PROJECT_CONF_DIR)
     project = getConf('project')
     args = parseArguments(project)
-    print("-- Configure '%s' project" % project.NAME)
+    print("-- Configure '%s' project." % project.NAME)
     build_env = BuildEnv(project, args)
 
     build_env.setup()
