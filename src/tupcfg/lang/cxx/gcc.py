9bd4aef646205a1eb5ca05664053e65d0b9f47be
Raphaël Londeix <raphael.londeix@gmail.com>
2012-10-13 15:21:03 +0200

    fix: set rpath

--- src/tupcfg/lang/cxx/gcc.py
+++ src/tupcfg/lang/cxx/gcc.py
@@ -3,15 +3,15 @@
 from tupcfg import Target
 from . import compiler
 
-            #from os.path import dirname, abspath
-            #closure = set()
-            #def add_rpath(lib, **kw):
-            #    p = '-Wl,-rpath=\\$ORIGIN/' + dirname(lib.relpath(kw['target'], **kw))
-            #    if p in closure:
-            #        return ''
-            #    else:
-            #        closure.add(p)
-            #        return p
+class LinkFlag:
+    def __init__(self, lib, method):
+        self.lib = lib
+        self.method = method
+
+    def shell_string(self, **kw):
+        return self.method(self.lib, **kw)
+
+
 class Compiler(compiler.Compiler):
 
     binary_name = 'g++'
@@ -46,18 +46,39 @@ class Compiler(compiler.Compiler):
             '-o', kw['target'],
         ]
 
+    def __add_rpath(self, lib, **kw):
+        from os.path import dirname
+        return '-Wl,-rpath=\\$ORIGIN/' + dirname(lib.relpath(kw['target'], **kw))
+
     def __get_link_flags(self, cmd, **kw):
         library_directories = set(self.library_directories)
         link_flags = []
+        class RPathFlag:
+            def __init__(self):
+                self.libs = []
+                self.directories = []
+
+            def shell_string(self, **kw):
+                from os.path import abspath, dirname
+                dirs = set(self.directories)
+                for lib in self.libs:
+                    dirs.add(dirname(abspath(lib.path(**kw))))
+                if dirs:
+                    return '-Wl,-rpath,' + ':'.join(dirs)
+                return ''
+
+        rpath = RPathFlag()
         for lib in cmd.libraries:
             if isinstance(lib, Target):
-                #self.linker_flags.append(self.LinkFlag(lib, add_rpath)) #XXX
                 link_flags.append(lib)
+                rpath.libs.append(lib)
             else:
                 for dir_ in lib.directories:
                     library_directories.add(dir_)
                 for name in lib.names:
                     link_flags.append('-l%s' % name)
+        rpath.directories.extend(library_directories)
+        link_flags.append(rpath)
         return list(('-L%s' % l) for l in library_directories) + link_flags
 
     def _link_executable_cmd(self, cmd, **kw):
@@ -69,10 +90,12 @@ class Compiler(compiler.Compiler):
         ]
 
     def _link_library_cmd(self, cmd, **kw):
+        from os.path import basename
         return [
             self.binary,
             cmd.dependencies,
             cmd.shared and '-shared' or '',
+            cmd.shared and '-Wl,-soname,' + basename(kw['target'].path(**kw)),
             '-o', kw['target'],
             self.__get_link_flags(cmd, **kw)
         ]
