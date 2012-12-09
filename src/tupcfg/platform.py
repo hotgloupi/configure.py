e6402d9137c0179d111f0cd9c8f82dc5583d900f
Raphaël Londeix <raphael.londeix@gmail.com>
2012-12-09 02:05:32 +0100

    add: platform detection factorized

---
+++ src/tupcfg/platform.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+
+import sys
+
+IS_WINDOWS = sys.platform.lower().startswith('win')
+IS_MACOSX = sys.platform.lower().startswith('darwin')
+IS_LINUX = sys.platform.lower().startswith('linux')
+
+# make sure the platform is recognized
+assert(IS_WINDOWS or IS_MACOSX or IS_LINUX)
+
+# make sure only one of them is true
+assert(int(IS_WINDOWS) + int(IS_MACOSX) + int(IS_LINUX) == 1)
+
+import platform
+
+ARCHITECTURE = platform.architecture()[0]
