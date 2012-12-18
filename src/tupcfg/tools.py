a88605d7d9e58c2a8697cc8ac63844b97962ac15
Raphael Londeix <Raphael.Londeix@gmail.com>
2012-12-18 09:42:30 +0100

    add: find_files()

--- src/tupcfg/tools.py
+++ src/tupcfg/tools.py
@@ -80,3 +80,56 @@ def glob(pattern, dir_=None, recursive=False):
 def isiterable(obj):
     return isinstance(obj, (list, tuple, types.GeneratorType))
 
+def _match_file(file_, name, extensions, prefixes, suffixes, validator):
+    from fnmatch import fnmatch
+    filename, ext = os.path.splitext(file_)
+    if extensions:
+        if len(ext):
+            assert ext[0] == '.'
+            ext = ext[1:]
+        if ext not in extensions:
+            return False
+    matching_prefixes = ['']
+    if prefixes:
+        matching_prefixes = list(p for p in prefixes if filename.startswith(p))
+        if not matching_prefixes:
+            return False
+
+    matching_suffixes = ['']
+    if suffixes:
+        matching_suffixes = list(p for p in suffixes if filename.endswith(p))
+        if not matching_suffixes:
+            return False
+
+    for p in matching_prefixes:
+        for s in matching_suffixes:
+            if len(p) + len(s) > len(filename):
+                continue
+            if fnmatch(filename[len(p):len(filename) - len(s)], name):
+                return True
+    return False
+
+
+
+
+def find_files(working_directory='.',
+               name=None,
+               extensions=None,
+               prefixes=None,
+               suffixes=None,
+               validator=None):
+
+    results = []
+    for root, dirnames, files in os.walk(working_directory):
+        results.extend(
+            file_ for file_ in files if _match_file(
+                file_,
+                name,
+                extensions,
+                prefixes,
+                suffixes,
+                validator
+            )
+        )
+    return results
+
