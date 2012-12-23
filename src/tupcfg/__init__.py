6aa1c2e301e28ce4c12a0ad305e91cdc9dff9c1d
Raphaël Londeix <raphael.londeix@gmail.com>
2012-12-23 16:08:14 +0100

    add: expose Generator class

--- src/tupcfg/__init__.py
+++ src/tupcfg/__init__.py
@@ -5,6 +5,7 @@ from . import tools, path
 from .build import Build
 from .command import Command
 from .env import Env
+from .generator import Generator
 from .node import Node
 from .project import Project
 from .source import Source
