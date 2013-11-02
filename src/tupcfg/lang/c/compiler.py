f4b89b9630bdd6f76163de9462e2303b9223dcea
Raphaël Londeix <raphael.londeix@infinit.io>
2013-11-02 23:50:05 +0100

    Fix Generic C compiler to use new Command interface.

--- src/tupcfg/lang/c/compiler.py
+++ src/tupcfg/lang/c/compiler.py
@@ -12,23 +12,21 @@ class Compiler(BaseCompiler):
         super().__init__(project, build, **kw)
 
     def build_object(self, src, **kw):
-        target = super(Compiler, self).build_object(src, **kw)
-        pchs = kw.get('precompiled_headers', []) + self.precompiled_headers
-        target.additional_inputs.extend(pchs)
+        target = super().build_object(src, **kw)
+        pchs = self.list_attr('precompiled_headers', kw = kw)
+        target.dependencies.extend(pchs)
         return target
 
     def generate_precompiled_header(self, source, force_include = False, **kw):
-        cpy = self.build.fs.copy(source)
-        command = self._generate_precompiled_header(
+        build = self.attr('build', kw)
+        cpy = build.fs.copy(source)
+        target = Target(build, source + '.gch')
+        command = self._build_object_cmd(
+            target,
             cpy,
             precompiled_header = True,
+            action = 'Build precompiled header',
             **kw
         )
-        target = Target(source + '.gch', command)
-        target.source = command.source
-        target.force_include = force_include
-        return self.build.add_target(target)
-
-    def _generate_precompiled_header(self, source, **kw):
-        raise Exception("Not implemented")
+        return target
 
