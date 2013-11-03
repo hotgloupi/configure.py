d6564bc1252b7da7412f95db1a8be62656ab068a
Raphaël Londeix <raphael.londeix@infinit.io>
2013-11-03 17:54:15 +0100

    Update and factor SDL dependencies definitions.

--- src/tupcfg/lang/c/libraries/sdl.py
+++ src/tupcfg/lang/c/libraries/sdl.py
@@ -3,7 +3,7 @@
 from ..library import Library
 from tupcfg import platform, path, tools
 from tupcfg import Dependency, Target
-from tupcfg.command import Shell as ShellCommand
+from tupcfg.command import Command
 
 class SDLLibrary(Library):
     def __init__(self, compiler, components=[], **kw):
@@ -25,10 +25,53 @@ class SDLLibrary(Library):
         return [self] + self.components
 
 
-class SDLDependency(Dependency):
-    def __init__(self, compiler, source_directory, shared = True):
-        super().__init__("SDL Library", "SDL")
-        self.source_directory = source_directory
+
+class _Dependency(Dependency):
+    __targets = None
+    @property
+    def install_directory(self):
+        return self.build_path('install')
+
+    @property
+    def absolute_install_directory(self):
+        return path.absolute(self.build.directory, self.install_directory)
+
+    @property
+    def library_directory(self):
+        if self.shared and platform.IS_WINDOWS:
+            dir = 'bin'
+        else:
+            dir = 'lib'
+        return path.join(self.install_directory, dir)
+
+    @property
+    def absolute_library_directory(self):
+        return path.absolute(self.build.directory, self.library_directory)
+
+    @property
+    def library_path(self):
+        return path.join(self.library_directory, self.library_filename)
+
+    @property
+    def absolute_library_path(self):
+        return path.absolute(self.build.directory, self.library_path)
+
+    @property
+    def compiler_binary(self):
+        if platform.IS_MACOSX:
+            # on macosx SDL does compile with the default compiler
+            return 'gcc'
+        return self.compiler.binary,
+
+    @property
+    def targets(self):
+        if self.__targets is None:
+            self.__targets = self._targets()
+        return self.__targets
+
+class SDLDependency(_Dependency):
+    def __init__(self, build, compiler, source_directory, shared = True):
+        super().__init__(build, "SDL", source_directory)
         self.shared = shared
         self.compiler = compiler
         ext = self.compiler.library_extension(shared)
@@ -39,32 +82,27 @@ class SDLDependency(Dependency):
         self.library_filename = '%sSDL2.%s' % (prefix, ext)
         self.__libraries = None
 
-    @property
-    def targets(self):
-        configure_script = path.absolute(self.source_directory, 'configure')
-        configure_target = Target(
-            self.build_path('build/Makefile'),
-            ShellCommand(
-                "Configure libSDL",
-                [
-                    configure_script,
-                    '--prefix', self.build_path('install', abs=True),
-                ],
-                working_directory = self.build_path('build'),
-                #env = {
-                #    'CC': self.compiler.binary,
-                #},
-            )
-        )
-        install_target = Target(
-            self.library_path,
-            ShellCommand(
-                "Installing %s" % self.name,
-                [self.resolved_build.make_program, 'install'],
-                working_directory = self.build_path('build'),
-                dependencies = [configure_target]
-            )
-        )
+    def _targets(self):
+        configure_script = self.absolute_source_path('configure')
+        configure_target = Command(
+            action = "Configure %s" % self.name,
+            target = Target(self.build, self.build_path('build/Makefile')),
+            command = [
+                configure_script,
+                '--prefix', self.absolute_build_path('install'),
+            ],
+            working_directory = self.build_path('build'),
+            env = {
+                'CC': self.compiler_binary,
+            },
+        ).target
+        install_target = Command(
+            action = "Install %s" % self.name,
+            target = Target(self.build, self.library_path),
+            command = [self.build.make_program, 'install'],
+            working_directory = self.build_path('build'),
+            inputs = [configure_target]
+        ).target
         return [install_target]
 
     @property
@@ -78,37 +116,18 @@ class SDLDependency(Dependency):
                 shared = self.shared,
                 search_binary_files = False,
                 include_directories = [
-                    self.build_path('install/include/SDL2', abs = True)
+                    self.absolute_build_path('install/include/SDL2')
                 ],
-                directories = [self.library_directory],
-                files = [self.library_path],
+                directories = [self.absolute_library_directory],
+                files = [self.absolute_library_path],
                 save_env_vars = False,
             )
         ]
         return self.__libraries
 
-    @property
-    def install_directory(self):
-        return self.build_path('install', abs = True)
-
-    @property
-    def library_directory(self):
-        if self.shared and platform.IS_WINDOWS:
-            return self.build_path('install/bin', abs = True)
-        else:
-            return self.build_path('install/lib', abs = True)
-
-    @property
-    def library_path(self):
-        if self.shared and platform.IS_WINDOWS:
-            return self.build_path('install/bin', self.library_filename, abs = True)
-        else:
-            return self.build_path('install/lib', self.library_filename, abs = True)
-
-class SDLImageDependency(Dependency):
-    def __init__(self, compiler, source_directory, sdl, shared = True):
-        super().__init__("SDL_image Library", "SDL_image")
-        self.source_directory = source_directory
+class SDLImageDependency(_Dependency):
+    def __init__(self, build, compiler, source_directory, sdl, shared = True):
+        super().__init__(build, "SDL_image", source_directory)
         self.shared = shared
         self.compiler = compiler
         ext = self.compiler.library_extension(shared)
@@ -120,39 +139,35 @@ class SDLImageDependency(Dependency):
         self.__sdl = sdl
         self.__libraries = None
 
-    @property
-    def targets(self):
+    def _targets(self):
         if platform.IS_WINDOWS and tools.which('make') is None:
             raise Exception("SDL_image cannot install properly without a make program available :/")
-        configure_script = path.absolute(self.source_directory, 'configure')
-        configure_target = Target(
-            self.build_path('build/Makefile'),
-            ShellCommand(
-                "Configure libSDL_image",
-                [
-                    configure_script,
-                    '--prefix', self.build_path('install', abs=True),
-                    '--with-sdl-prefix=%s' % self.__sdl.install_directory,
-                    '--disable-sdltest',
-                ],
-                working_directory = self.build_path('build'),
-            #    env = {
-                    #'CC': self.compiler.binary,
-                    # on windows, passing MAKE env var generate errorneous makefiles...
-                    #'MAKE': path.basename(self.compiler.build.make_program),
-             #   },
-                dependencies = self.__sdl.targets,
-            )
-        )
-        install_target = Target(
-            self.library_path,
-            ShellCommand(
-                "Installing %s" % self.name,
-                [self.resolved_build.make_program, 'install'],
-                working_directory = self.build_path('build'),
-                dependencies = [configure_target],
-            )
-        )
+        configure_script = self.absolute_source_path('configure')
+        configure_target = Command(
+            action = "Configure %s" % self.name,
+            target = Target(self.build, self.build_path('build/Makefile')),
+            command = [
+                configure_script,
+                '--prefix', self.absolute_build_path('install'),
+                '--with-sdl-prefix=%s' % self.__sdl.absolute_install_directory,
+                '--disable-sdltest',
+                '--disable-webp',
+            ],
+            working_directory = self.build_path('build'),
+            env = {
+                'CC': self.compiler_binary,
+                # on windows, passing MAKE env var generate errorneous makefiles...
+                #'MAKE': path.basename(self.compiler.build.make_program),
+            },
+            inputs = self.__sdl.targets,
+        ).target
+        install_target = Command(
+            action = "Install %s" % self.name,
+            target = Target(self.build, self.library_path),
+            command = [self.build.make_program, 'install'],
+            working_directory = self.build_path('build'),
+            inputs = [configure_target],
+        ).target
         return [install_target]
 
     @property
@@ -166,30 +181,11 @@ class SDLImageDependency(Dependency):
                 shared = self.shared,
                 search_binary_files = False,
                 include_directories = [
-                    self.build_path('install/include/SDL2', abs = True)
+                    self.absolute_build_path('install/include/SDL2')
                 ],
-                directories = [self.library_directory],
-                files = [self.library_path],
+                directories = [self.absolute_library_directory],
+                files = [self.absolute_library_path],
                 save_env_vars = False,
             )
         ]
         return self.__libraries
-
-
-    @property
-    def install_directory(self):
-        return self.build_path('install', abs = True)
-
-    @property
-    def library_directory(self):
-        if self.shared and platform.IS_WINDOWS:
-            return self.build_path('install/bin', abs = True)
-        else:
-            return self.build_path('install/lib', abs = True)
-
-    @property
-    def library_path(self):
-        if self.shared and platform.IS_WINDOWS:
-            return self.build_path('install/bin', self.library_filename, abs = True)
-        else:
-            return self.build_path('install/lib', self.library_filename, abs = True)
