01b7fae19eb28e90aad384f168ee938eb4787cb4
Raphaël Londeix <raphael.londeix@infinit.io>
2013-11-16 15:51:40 +0100

    lang.cxx.clang: Set default path of the libc++ relatively to the compiler location.

--- src/tupcfg/lang/cxx/clang.py
+++ src/tupcfg/lang/cxx/clang.py
@@ -1,7 +1,7 @@
 
 from . import gcc
 
-from tupcfg import platform
+from tupcfg import platform, path
 
 class Compiler(gcc.Compiler):
     name = 'clang'
@@ -14,12 +14,14 @@ class Compiler(gcc.Compiler):
         )
         super().__init__(project, build, **kw)
         if platform.IS_MACOSX and self.stdlib == 'libc++':
+            prefix = path.join(path.dirname(self.binary), '..', absolute = True)
             self.libraries.extend([
                 self.find_library(
                     'libc++',
                     name_suffixes = ['', '.1'],
                     include_directory_names = ['../lib/c++/v1'],
-                    find_includes = ['__config']
+                    find_includes = ['__config'],
+                    prefixes = [prefix]
                 ),
                 #self.find_library('libc++abi'),
             ])
