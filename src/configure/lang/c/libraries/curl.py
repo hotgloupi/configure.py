db9a6f84f6a53c1f3b28bf77550abf08d7d17cee
Raphaël Londeix <raphael.londeix@infinit.io>
2014-04-12 15:40:08 +0200

    lang.c.libraries.curl: Compat with MSVC.

--- src/configure/lang/c/libraries/curl.py
+++ src/configure/lang/c/libraries/curl.py
@@ -1,8 +1,79 @@
 
 from configure.dependency.cmake import CMakeDependency
 from configure import platform
+from configure.dependency import Dependency
+from configure.command import Command
+from configure.target import Target
 
-class CURLDependency(CMakeDependency):
+class CURLDependencyMSVC(Dependency):
+    def __init__(self,
+                 build,
+                 compiler,
+                 source_directory,
+                 zlib = None,
+                 openssl = None,
+                 with_cookies = True,
+                 with_crypto_auth = True,
+                 with_dict = True,
+                 with_file = True,
+                 with_ftp = True,
+                 with_http = True,
+                 with_ldap = True,
+                 with_ldaps = True,
+                 with_telnet = True,
+                 with_tftp = True,
+                 use_ares = False,
+                 hidden_visibility = True,
+                 shared = True):
+        super().__init__(
+            build,
+            name = 'curl',
+            source_directory = source_directory,
+        )
+        command = [
+            'nmake', '/f', 'Makefile.vc',
+            'ENABLE_IDN=no', 'ENABLE_WINSSL=no', 'GEN_PDB=no', 'DEBUG=no',
+            'mode=%s' % (shared and 'dll' or 'static'),
+        ]
+        dir = 'src/builds/libcurl-vc-x86-release-%(mode)s-ipv6-sspi-spnego' % {
+            'mode': shared and 'dll' or 'static',
+        }
+        if shared:
+            lib = dir + '/bin/libcurl.dll'
+            lnk = dir + '/lib/libcurl.lib'
+        else:
+            lib = dir + '/lib/libcurl_a.lib'
+            lnk = lib
+        target = Target(self.build, self.build_path(lib))
+        self.targets = [
+            Command(
+                "Building %s" % self.name,
+                target = target,
+                additional_outputs = [],
+                command = command,
+                working_directory = self.build_path('src/winbuild'),
+                inputs = build.fs.copy_tree(
+                    self.source_path(),
+                    dest = self.build_path('src')
+                ),
+                os_env = compiler.os_env,
+            ).target
+        ]
+        self.libraries = [
+            compiler.Library(
+                self.name,
+                compiler,
+                shared = shared,
+                search_binary_files = False,
+                include_directories = [self.absolute_source_path('include')],
+                directories = [self.absolute_build_path(dir, 'lib')],
+                files = [self.absolute_build_path(lib)],
+                link_files = [self.absolute_build_path(lnk)],
+                save_env_vars = False,
+            )
+        ]
+
+class CURLDependencyCMake(CMakeDependency):
 
     def __init__(self,
                  build,
@@ -55,6 +126,7 @@ class CURLDependency(CMakeDependency):
                     'prefix': 'lib',
                     'name': 'curl',
                     'shared': shared,
+                    'imp_filename': (shared and compiler.name == 'msvc') and 'libcurl_imp.lib' or 'libcurl.lib',
                 }
             ],
             configure_variables = configure_variables
@@ -63,3 +135,8 @@ class CURLDependency(CMakeDependency):
             from configure.lang import c
             self.libraries.append(c.libraries.simple('idn', compiler, system = True))
 
+
+if platform.IS_WINDOWS and False:
+    CURLDependency = CURLDependencyMSVC
+else:
+    CURLDependency = CURLDependencyCMake
