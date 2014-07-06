4de94ce23fd97ef1d74d96469aa9177a09f25cc8
Raphaël Londeix <raphael.londeix@infinit.io>
2014-07-06 18:21:03 +0200

    Add project unittests and doc.

--- src/configure/project.py
+++ src/configure/project.py
@@ -13,28 +13,54 @@ from .build import Build
 class Project:
 
     @classmethod
-    def initialize(self, config_directory, config_filename):
-        """Prepare a project skeleton."""
+    def initialize(cls,
+                   directory,
+                   config_directory = '.config',
+                   config_filename = 'project.py',
+                   template = True):
+        """Prepare a project skeleton and return a project instance.
+        positional arguments:
+
+            directory: Where the project lies.
+
+        optional arguments:
+
+            config_directory: Directory name for configuration.
+            config_filename: Project configuration file name.
+            template: If given as a string, will be used as project
+                      configuration, otherwise if it evaluate to True, the
+                      default project configuration template is used.
+        """
+        config_directory = path.join(directory, config_directory)
         config_file = path.join(config_directory, config_filename)
         if path.exists(config_file):
             tools.fatal(
                 "This project seems to be already initialized, see '%s'" % path.clean(config_file, replace_home = True)
             )
         if not path.exists(config_directory):
-            os.makedirs(config_directory)
-        with open(templates.project()) as template:
-            data = template.read()
+            os.mkdir(config_directory)
+        if isinstance(template, str):
+            data = template
+        elif template:
+            with open(templates.project()) as template:
+                data = template.read()
         with open(config_file, 'w') as conf:
             conf.write(data)
+        return Project(
+            directory = directory,
+            config_directory = config_directory,
+            config_filename = config_filename
+        )
 
-    def __init__(self, directory, config_dir,
+    def __init__(self, directory,
+                 config_directory = '.config',
                  config_filename = "project.py",
                  build_env_filename = ".build-env",
                  project_env_filename = ".project-env",
                  new_project_env = {}):
         self.directory = path.absolute(directory)
-        self.config_directory = config_dir
-        self.config_file = path.join(config_dir, config_filename)
+        self.config_directory = path.join(self.directory, config_directory)
+        self.config_file = path.join(self.config_directory, config_filename)
         self.build_env_filename = build_env_filename
         self.project_env_filename = project_env_filename
 
@@ -62,12 +88,12 @@ class Project:
                      build_dir,
                      new_build_env,
                      configure_function,
-                     generator_names):
+                     generator_name):
             self.project = project
             self.build_dir = build_dir
             self.configure_function  = configure_function
             self.new_build_env = new_build_env
-            self.generator_names = generator_names
+            self.generator_name = generator_name
             self.build = None
             self.build_env_file = path.join(
                 self.build_dir,
@@ -84,8 +110,8 @@ class Project:
             env.update(self.new_build_env)
             self.build = Build(
                 self.project,
-                self.build_dir,
-                self.generator_names,
+                directory = self.build_dir,
+                generator_name = self.generator_name,
                 env = env
             )
             self.configure_function(self.build)
@@ -95,17 +121,19 @@ class Project:
             self.build.env.save(self.build_env_file)
             self.build.cleanup()
 
-    def configure(self, build_dir, new_build_vars, generator_names):
+    def configure(self, build_dir, new_build_vars = {}, generator_name = None):
         if self.__configure_function is None:
             raise Exception(
                 "The project file %s did not define any `main` function"
             )
+        if not path.is_absolute(build_dir):
+            build_dir = path.join(self.directory, build_dir)
         return self.BuildProxy(
             self,
             build_dir,
             new_build_vars,
             self.__configure_function,
-            generator_names,
+            generator_name = generator_name,
         )
 
     def __read_conf(self):
@@ -125,4 +153,49 @@ class Project:
                     self.__configure_function = v
         finally:
             sys.path = backup
+
+import tempfile
+
+class TemporaryProject:
+    """Test class for projects"""
+
+    def __init__(self, config = ''):
+        self.config = config
+
+    def __enter__(self):
+        self.tempdir = tempfile.TemporaryDirectory()
+        self.project = Project.initialize(
+            directory = self.tempdir.name,
+            template = self.config,
+        )
+        return self.project
+
+    def __exit__(self, *args):
+        self.tempdir.cleanup()
+
+
+from unittest import TestCase
+import textwrap
+
+class _(TestCase):
+
+    empty_config = textwrap.dedent(
+        """
+        def main(build):
+            pass
+        """
+    )
+
+    def test_init(self):
+        with TemporaryProject() as p:
+            self.assertTrue(os.path.isdir(p.directory))
+            self.assertTrue(os.path.isdir(p.config_directory))
+
+    def test_configure(self):
+        with TemporaryProject(config = self.empty_config) as p:
+            build_dir = path.join(p.directory, 'build')
+            os.mkdir(build_dir)
+            with p.configure(build_dir, generator_name = 'Makefile') as build:
+                pass
+            self.assertTrue(os.path.isfile(path.join(build_dir, '.build-env')))
 
