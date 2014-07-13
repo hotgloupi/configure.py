202bf5b4435790fef8b6db83c5f8147113743eaf
Raphaël Londeix <raphael.londeix@infinit.io>
2014-07-13 11:51:45 +0200

    lang.cxx.clang: Disable auto stdlib search.

--- src/configure/lang/cxx/clang.py
+++ src/configure/lang/cxx/clang.py
@@ -13,17 +13,17 @@ class Compiler(gcc.Compiler, c_clang.Compiler):
             kw.setdefault('stdlib', 'libc++')
         kw.setdefault('standard', 'c++11')
         super().__init__(build, **kw)
-        if platform.IS_MACOSX and self.stdlib == 'libc++':
-            prefix = path.join(path.dirname(self.binary), '..', absolute = True)
-            self.libraries.extend([
-                self.find_library(
-                    'libc++',
-                    name_suffixes = ['', '.1'],
-                    include_directory_names = ['../lib/c++/v1'],
-                    find_includes = ['__config'],
-                    prefixes = [prefix]
-                ),
-                #self.find_library('libc++abi'),
-            ])
+        #if platform.IS_MACOSX and self.stdlib == 'libc++':
+        #    prefix = path.join(path.dirname(self.binary), '..', absolute = True)
+        #    self.libraries.extend([
+        #        self.find_library(
+        #            'libc++',
+        #            name_suffixes = ['', '.1'],
+        #            include_directory_names = ['../lib/c++/v1'],
+        #            find_includes = ['__config'],
+        #            prefixes = [prefix]
+        #        ),
+        #        #self.find_library('libc++abi'),
+        #    ])
 
 
