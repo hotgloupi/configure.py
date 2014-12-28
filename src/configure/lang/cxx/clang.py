87f8727498b7d159bc4d3e6fb0cf7970f2f92029
Raphaël Londeix <raphael.londeix@infinit.io>
2014-12-28 17:40:26 +0100

    lang.cxx.clang: Fix libc++ search.

--- src/configure/lang/cxx/clang.py
+++ src/configure/lang/cxx/clang.py
@@ -14,14 +14,20 @@ class Compiler(gcc.Compiler, c_clang.Compiler):
         kw.setdefault('standard', 'c++11')
         super().__init__(build, **kw)
         if platform.IS_OSX and self.stdlib == 'libc++':
-            prefix = path.join(path.dirname(self.binary), '..', absolute = True)
+            prefix = path.join(path.dirname(path.absolute(self.binary, real = True)), '..', absolute = True)
             self.libraries.extend([
                 self.find_library(
                     'libc++',
                     name_suffixes = ['', '.1'],
-                    include_directory_names = ['../lib/c++/v1'],
-                    find_includes = ['__config'],
-                    prefixes = [prefix]
+                    include_directories = [
+                        path.join(prefix, 'lib/c++/v1'),
+                        path.join(prefix, 'include/c++/v1'),
+                    ],
+                    find_includes = ['__config', 'cxxabi.h'],
+                    prefixes = [
+                        prefix,
+                        path.join(prefix, 'usr')
+                    ]
                 ),
                 #self.find_library('libc++abi'),
             ])
