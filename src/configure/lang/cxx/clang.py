5becebd5c5b51748100fed46400e25c7d634b709
Raphaël Londeix <raphael.londeix@infinit.io>
2014-12-28 15:49:33 +0100

    lang.cxx.clang: Add workaround for os x 10.6.

--- src/configure/lang/cxx/clang.py
+++ src/configure/lang/cxx/clang.py
@@ -26,4 +26,11 @@ class Compiler(gcc.Compiler, c_clang.Compiler):
                 #self.find_library('libc++abi'),
             ])
 
+    def _get_build_flags(self, kw):
+        flags = super()._get_build_flags(kw)
+        if platform.IS_OSX and platform.OSX_VERSION_MAJOR < 10 and or \
+          (platform.OSX_VERSION_MAJOR == 10 and platform.OSX_VERSION_MINOR <= 6):
+            # Bug in math.h on OS X 10.6
+            flags.append('-U__STRICT_ANSI__')
+        return flags
 
